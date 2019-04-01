799644ca776e8bcab30aa659c41172483f8c37fd
Nicanor008 <nicanorkorir008@gmail.com>
2019-04-01 22:05:19 +0300

    nav bar

--- mentorship-101/src/App.js
+++ mentorship-101/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import NavBar from './components/navBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Mentorship 101</h2>
         </header>
       </div>
     );
